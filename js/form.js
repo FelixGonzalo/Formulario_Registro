0a019db39af9405392c1c25e7b4d011e3be4f91a
Felix Castro <felixgonzalocastro@gmail.com>
2020-12-13 03:12:04 -0500

    demo1

---
+++ js/form.js
@@ -0,0 +1,71 @@
+const nombre = document.getElementById("myname");
+const apellidos = document.getElementById("surname");
+const correo = document.getElementById("email");
+const celular = document.getElementById("mobile");
+const contrasenia = document.getElementById("password");
+const contrasenia2 = document.getElementById("repeatPassword");
+const terminosYcondiciones = document.getElementById("termsAndConditions");
+const form = document.getElementById("form");
+const listInputs = document.querySelectorAll(".form-input");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let condicion = validacionForm();
+  if (condicion) {
+    enviarFormulario();
+  }
+});
+
+function validacionForm() {
+  form.lastElementChild.innerHTML = "";
+  let condicion = true;
+  listInputs.forEach((element) => {
+    element.lastElementChild.innerHTML = "";
+  });
+
+  if (nombre.value.length < 1 || nombre.value.trim() == "") {
+    mostrarMensajeError("myname", "Nombre no valido*");
+    condicion = false;
+  }
+  if (apellidos.value.length < 1 || apellidos.value.trim() == "") {
+    mostrarMensajeError("surname", "Apellido no valido");
+    condicion = false;
+  }
+  if (correo.value.length < 1 || correo.value.trim() == "") {
+    mostrarMensajeError("email", "Correo no valido*");
+    condicion = false;
+  }
+  if (
+    celular.value.length != 9 ||
+    celular.value.trim() == "" ||
+    isNaN(celular.value)
+  ) {
+    mostrarMensajeError("mobile", "Celular no valido*");
+    condicion = false;
+  }
+  if (contrasenia.value.length < 1 || contrasenia.value.trim() == "") {
+    mostrarMensajeError("password", "Contraseña no valido*");
+    condicion = false;
+  }
+  if (contrasenia2.value != contrasenia.value) {
+    mostrarMensajeError("repeatPassword", "Contraseña error*");
+    condicion = false;
+  }
+  if (!terminosYcondiciones.checked) {
+    mostrarMensajeError("termsAndConditions", "Acepte*");
+    condicion = false;
+  } else {
+    mostrarMensajeError("termsAndConditions", "");
+  }
+  return condicion;
+}
+
+function mostrarMensajeError(claseInput, mensaje) {
+  let elemento = document.querySelector(`.${claseInput}`);
+  elemento.lastElementChild.innerHTML = mensaje;
+}
+
+function enviarFormulario() {
+  form.reset();
+  form.lastElementChild.innerHTML = "Listo !!";
+}
